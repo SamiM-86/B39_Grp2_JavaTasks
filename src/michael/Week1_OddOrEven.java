8870e3e349f36ba9eb0baeedfefbb5c09be4b5d8
Ersin <michael.knight@cydeo.com>
2025-04-29 23:12:22 +0300

    OddOrEven Method is created...

---
+++ src/michael/Week1_OddOrEven.java
@@ -0,0 +1,13 @@
+package michael;
+
+public class Week1_OddOrEven {
+    public static void main(String[] args) {
+
+    }
+
+    public static void oddOrEven(int num){
+        if(num%2==0){
+            System.out.println(num + " is an Even number");
+        }
+    }
+}
