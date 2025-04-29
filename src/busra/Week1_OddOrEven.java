4168e22c89c207d0ea76e125f2e5521a755ffb2a
Büşra Taş <busrabelkistas@gmail.com>
2025-04-29 22:26:09 +0300

    Week1_OddOrEven Class is done.

--- src/busra/Week1_OddOrEven.java
+++ src/busra/Week1_OddOrEven.java
@@ -1,4 +1,25 @@
 package busra;
 
 public class Week1_OddOrEven {
+
+    public static void main(String[] args) {
+
+        oddOrEven(5);
+        oddOrEven(-5);
+        oddOrEven(0);
+        oddOrEven(10);
+        oddOrEven(566);
+
+    }
+
+    public static void oddOrEven(int num) {
+
+        if (num == 0) {
+            System.out.println(num + " is zero.");
+        } else if (num % 2 == 0){
+            System.out.println(num + " is an even number.");
+        } else {
+            System.out.println(num + " is an odd number.");
+        }
+    }
 }
