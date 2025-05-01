47e5ea793ad9a6d506c2c3be48718937e5150421
atlas <yasin_atlas_24@hotmail.com>
2025-05-01 23:18:48 +0300

    Week1_OddOrEven

---
+++ src/yasin/Week1_OddOrEven.java
@@ -0,0 +1,22 @@
+package yasin;
+
+public class Week1_OddOrEven {
+
+    public static void main(String[] args) {
+
+
+    }
+    public static void oddOrEven(int num) {
+
+        if (num == 0){
+            System.out.println(num + "is zero number");
+        }
+        else if (num%2 == 0) {
+            System.out.println(num + "is even number");
+        }
+        else {
+            System.out.println(num + "is odd number");
+        }
+    }
+
+}
