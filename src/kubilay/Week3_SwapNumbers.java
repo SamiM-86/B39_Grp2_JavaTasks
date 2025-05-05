cf5755a0fdee03890a596b81dd52465f22e53c91
Kubilay2701 <kubilaydemircioglu@gmail.com>
2025-05-05 23:06:30 +0300

    Week3_SwapNumbers Task is done.

---
+++ src/kubilay/Week3_SwapNumbers.java
@@ -0,0 +1,20 @@
+package kubilay;
+
+public class Week3_SwapNumbers {
+
+    public static void main(String[] args) {
+
+        int num1 = 3;
+        int num2 = 7;
+
+        num1 = num1 + num2;
+
+        num2 = num1 - num2;
+
+        num1 = num1 - num2;
+
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+
+    }
+}
