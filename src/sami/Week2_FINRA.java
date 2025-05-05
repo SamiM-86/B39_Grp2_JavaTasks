22ceb6da43132d883afd1cc8b658c14b5b5b7297
samim <samimutlu@live.com>
2025-05-05 22:31:30 +0200

    Week2_FINRA class is created and FINRA method loop prepared and tested...

--- src/sami/Week2_FINRA.java
+++ src/sami/Week2_FINRA.java
@@ -4,12 +4,22 @@ public class Week2_FINRA {
 
     public static void main(String[] args) {
 
-
+        FINRA();
 
     }
 
-    public static void finra() {
-
+    public static void FINRA() {
+        for (int i = 1; i <= 30; i++) {
+            if (i % 15 == 0) {
+                System.out.print("FINRA ");
+            } else if (i % 3 == 0) {
+                System.out.print("FIN ");
+            } else if (i % 5 == 0) {
+                System.out.print("RA ");
+            } else {
+                System.out.print(i + " ");
+            }
+        }
 
 
     }
